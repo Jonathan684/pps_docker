4ee987f57692635da3f157ccba66ac449b6f71d4
jonathan684 <jonathan.lafalda@gmail.com>
2023-07-04 09:22:21 +0000

    update

--- iio_info/buscar_info.sh
+++
@@ -1,4 +0,0 @@
-#Buscar el archivo.
-#touch salida.txt
-#iio_info >> salida.txt
-sshpass -p "analog" scp  root@192.168.1.32:/jonathan/salida.txt ../iio_info
--- iio_info/generar.sh
+++
@@ -1,4 +0,0 @@
-#touch salida.txt
-#iio_info >> salida.txt
-sshpass -p "analog" ssh -t root@192.168.1.32 "cd /jonathan/; touch salida.txt ; iio_info >> salida.txt"
-sshpass -p "analog" scp  root@192.168.1.32:/jonathan/salida.txt ../iio_info
